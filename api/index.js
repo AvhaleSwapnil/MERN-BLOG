a2aaf913f2e2f7e9ba62ba419197e5602564c0a4
AvhaleSwapnil <swapnilavhale460@gmail.com>
2024-06-18 09:43:36 +0530

    connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MONGODB CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
